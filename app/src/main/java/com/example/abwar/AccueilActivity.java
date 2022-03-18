94f8eb3defc3562a9b19af118a63767240d8bf51
Snowballe <62845660+Snowballe@users.noreply.github.com>
2022-03-18 11:57:56 +0100

    trying to get sharedpreferences to work + retreving pseudos

--- app/src/main/java/com/example/abwar/AccueilActivity.java
+++ app/src/main/java/com/example/abwar/AccueilActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class AccueilActivity extends AppCompatActivity {
     private Button BtnToGame;
@@ -25,6 +28,9 @@ public class AccueilActivity extends AppCompatActivity {
     SharedPreferences mesjoueurs= getSharedPreferences("MesJoueurs",0);
     SharedPreferences.Editor editor=mesjoueurs.edit();
 
+        EditText et = new EditText(AccueilActivity.this);
+
+
         BtnToGame=(Button) findViewById(R.id.BtnToGame);
         BtnAdd=(Button) findViewById(R.id.BtnAdd);
 
@@ -32,16 +38,23 @@ public class AccueilActivity extends AppCompatActivity {
 
         TemplateEditText=(EditText)findViewById(R.id.editTextTextPersonName1);
 
-        EditText et = new EditText(AccueilActivity.this);
 
         BtnToGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i=0;i<LayoutNoms.getChildCount();i++){
-                    System.out.println(i);
-                }
+                ArrayList<String>TousMesjoueurs=new ArrayList<>();
 
+                for (int i=0;i<LayoutNoms.getChildCount();i++){
+                    EditText et = new EditText(AccueilActivity.this);
+
+                    Resources resourcesFromText= LayoutNoms.getChildAt(i).getResources();
+                   String joueur= (String) resourcesFromText.getText(LayoutNoms.getChildAt(i));
+                   TousMesjoueurs.add(joueur);
+                    editor.putInt(joueur,0);
+                    editor.commit();
+                }
                 Intent GoToGameActivity=new Intent(AccueilActivity.this, GameActivity.class);
+                GoToGameActivity.putExtra("joueurs",TousMesjoueurs);
                 startActivity(GoToGameActivity);
             }
         });
@@ -50,6 +63,9 @@ public class AccueilActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText et = new EditText(AccueilActivity.this);
+
+                int nbJoueur=LayoutNoms.getChildCount()+1;
+                et.setHint("Joueur "+nbJoueur);
                 LayoutNoms.addView(et);
             }
         });
