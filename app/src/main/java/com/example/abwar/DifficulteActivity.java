2a9ed9a1a40773b8a9fb207e5d893d952d0e9297
Snowballe <62845660+Snowballe@users.noreply.github.com>
2022-04-19 04:58:13 +0200

    adding code-behind for difficulty + starting out on the game

--- app/src/main/java/com/example/abwar/DifficulteActivity.java
+++ app/src/main/java/com/example/abwar/DifficulteActivity.java
@@ -2,13 +2,78 @@ package com.example.abwar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class DifficulteActivity extends AppCompatActivity {
+
+    private Button BtnEasy;
+    private Button btnIntermediate;
+    private Button btnHard;
+    private Button BtnHommeDeTrois;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulte);
+
+        ArrayList<String> mesJoueursPourLactivity=getIntent().getStringArrayListExtra("ACCES_JOUEURS");//Obligé de se le refiler pour pouvoir garder les suites de pseudos
+
+        BtnEasy=findViewById(R.id.BtnEasy);
+        btnIntermediate=findViewById(R.id.btnIntermediate);
+        btnHard=findViewById(R.id.btnHard);
+        BtnHommeDeTrois=findViewById(R.id.BtnHommeDeTrois);
+
+
+
+
+        BtnEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GotoGameActivity=new Intent(DifficulteActivity.this, GameActivity.class);
+                GotoGameActivity.putExtra("ACCES_JOUEURS",mesJoueursPourLactivity);
+                GotoGameActivity.putExtra("MaxRandInt",3);
+                GotoGameActivity.putExtra("MinRandInt",1);
+                startActivity(GotoGameActivity);
+            }
+        });
+
+        btnIntermediate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GotoGameActivity=new Intent(DifficulteActivity.this, GameActivity.class);
+                GotoGameActivity.putExtra("ACCES_JOUEURS",mesJoueursPourLactivity);
+                GotoGameActivity.putExtra("MaxRandInt",6);
+                GotoGameActivity.putExtra("MinRandInt",1);
+                startActivity(GotoGameActivity);
+            }
+        });
+
+        btnHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GotoGameActivity=new Intent(DifficulteActivity.this, GameActivity.class);
+                GotoGameActivity.putExtra("ACCES_JOUEURS",mesJoueursPourLactivity);
+                GotoGameActivity.putExtra("MaxRandInt",10);
+                GotoGameActivity.putExtra("MinRandInt",2);
+                startActivity(GotoGameActivity);
+            }
+        });
+
+        BtnHommeDeTrois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GotoTroisActivity=new Intent(DifficulteActivity.this, TroisActivity.class);
+                GotoTroisActivity.putExtra("ACCES_JOUEURS",mesJoueursPourLactivity);
+                startActivity(GotoTroisActivity);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/abwar/GameActivity.java
+++ app/src/main/java/com/example/abwar/GameActivity.java
@@ -2,8 +2,11 @@ package com.example.abwar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        ArrayList<String> mesJoueursPourLactivity=getIntent().getStringArrayListExtra("ACCES_JOUEURS");
+        int MaxInt=getIntent().getIntExtra("MaxRandInt",0);
+        int MinInt=getIntent().getIntExtra("MinRandInt",0);
+
+        SharedPreferences mesJoueurs = getSharedPreferences("MesJoueurs", 0);
+        SharedPreferences.Editor editor = mesJoueurs.edit();
+
+
+
     }
 }
