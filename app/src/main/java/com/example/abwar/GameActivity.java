9aba015dca1e52892989d10078e31a82c89271d2
Snowballe <62845660+Snowballe@users.noreply.github.com>
2022-04-21 19:30:46 +0200

    having random questions done, still need to delete them from the array

--- app/src/main/java/com/example/abwar/GameActivity.java
+++ app/src/main/java/com/example/abwar/GameActivity.java
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 private TextView textViewQuestion;
 private String[] questions;
+private Button BtnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
         textViewQuestion=findViewById(R.id.textViewQuestion);
+        BtnNext=findViewById(R.id.BtnNext);
 
         ArrayList<String> mesJoueursPourLactivity=getIntent().getStringArrayListExtra("ACCES_JOUEURS");
         int MaxInt=getIntent().getIntExtra("MaxRandInt",0);
@@ -27,7 +35,19 @@ private String[] questions;
         SharedPreferences.Editor editor = mesJoueurs.edit();
 
         questions=getResources().getStringArray(R.array.Questions);
+        List<String> questionsAryL= Arrays.asList(questions);
 
+        int randomIndex = new Random().nextInt(questions.length);
+        textViewQuestion.setText(questions[randomIndex]);
 
+        BtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewQuestion.setText("");
+                int randomIndex = new Random().nextInt(questions.length);
+                textViewQuestion.setText(questions[randomIndex]);
+                //questionsAryL.remove(randomIndex);
+            }
+        });
     }
 }
